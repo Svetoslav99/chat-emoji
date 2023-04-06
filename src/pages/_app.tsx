b835f94221d169350bcde91156f8059cb40a584b
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2023-04-06 20:44:56 +0300

    Posting loading states

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { type AppType } from "next/app";
-
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 import { api } from "y/utils/api";
 
 import "y/styles/globals.css";
@@ -8,6 +8,7 @@ import "y/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-center"/>
       <Component {...pageProps} />
     </ClerkProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import { SignInButton, useUser } from "@clerk/nextjs";
-import { api, RouterOutputs } from "y/utils/api";
-
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage } from "y/components/loading";
 import { contextProps } from "@trpc/react-query/shared";
+import { LoadingPage, LoadingSpinner } from "y/components/loading";
+import { api, RouterOutputs } from "y/utils/api";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -23,6 +23,15 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Faild to post! Please use only emojis!");
+      }
     },
   });
 
@@ -46,9 +55,24 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
 
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
